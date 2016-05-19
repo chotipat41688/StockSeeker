250d8c794f68406127d77d11022d8f68eff14f3f
YorTz <sincere_yortz@hotmail.com>
2016-05-20 02:27:25 +0700

    update getnews.php

--- getnews.php
+++ getnews.php
@@ -1,1 +1,21 @@
-getnews.php
+<?php 
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+	$db = "se";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $db);
+
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+	echo "Connected successfully<br>";
+	if ($result = $conn->query("SELECT * FROM news")) {
+	    while($row = $result->fetch_array(MYSQL_ASSOC)) {
+            $myArray[] = $row;
+    	}
+    	echo json_encode($myArray);
+	}
+ ?>
