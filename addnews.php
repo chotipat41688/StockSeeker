25c859f53489d8c58ebeb039f3b7a6f61fb425e3
YorTz <sincere_yortz@hotmail.com>
2016-05-20 10:46:19 +0700

    update addnews

--- addnews.php
+++ addnews.php
@@ -75,7 +75,7 @@
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $db);
-
+	$conn->query("SET NAMES UTF8");
 	// Check connection
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
