66630cec44b9444df1197a28f8c60ccb189a99c9
YorTz <sincere_yortz@hotmail.com>
2016-05-20 10:02:38 +0700

    update addnews

--- addnews.php
+++ addnews.php
@@ -1,1 +1,91 @@
-addnews.php
+<?php 
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+	function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+	}
+
+
+	$title = $_POST['title'];
+	//$image = $_POST['image'];
+	$subTitle = $_POST['subTitle'];
+	$detail = $_POST['detail'];
+	$type = $_POST['type'];
+	echo "Hi";
+	echo $title;
+	echo generateRandomString();
+
+	$target_dir = "imgs/";
+	$target_file = $target_dir .generateRandomString(). basename($_FILES["image"]["name"]);
+	echo '<br>'.$target_file;
+	$uploadOk = 1;
+	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submit"])) {
+	    $check = getimagesize($_FILES["image"]["tmp_name"]);
+	    if($check !== false) {
+	        echo "File is an image - " . $check["mime"] . ".";
+	        $uploadOk = 1;
+	    } else {
+	        echo "File is not an image.";
+	        $uploadOk = 0;
+	    }
+	}
+	// Check if file already exists
+	if (file_exists($target_file)) {
+	    echo "Sorry, file already exists.";
+	    $uploadOk = 0;
+	}
+	// Check file size
+	if ($_FILES["image"]["size"] > 500000) {
+	    echo "Sorry, your file is too large.";
+	    $uploadOk = 0;
+	}
+	// Allow certain file formats
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+	&& $imageFileType != "gif" ) {
+	    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	    $uploadOk = 0;
+	}
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+	    echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+	} else {
+	    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+	        echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
+	    } else {
+	        echo "Sorry, there was an error uploading your file.";
+	    }
+	}
+
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+	$db = "se";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $db);
+
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+
+
+	$query = "INSERT INTO news VALUES (NULL, '$title', '$subTitle', '$detail', '$target_file', $type)";
+	echo $query;
+	$conn->query($query);
+
+
+
+ ?>
